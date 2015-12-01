003778f8de6e7a1daead2a0f00dcaf4cdee6f97c
James Stephenson <jstephen@gmail.com>
2015-12-01 23:06:11 +0000

    Fix Client controller pagination

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,7 +4,7 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
-    @clients = Client.order(params[:sort]).paginate(:page => params[:page], per_page: 1)
+    @clients = Client.order(params[:sort]).paginate(:page => params[:page], per_page: 10)
   end
 
   # GET /clients/1
