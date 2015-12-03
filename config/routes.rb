4a9382416330055c1f64e04a2fa2233067a41f85
James Stephenson <jstephen@gmail.com>
2015-12-03 23:35:26 +0000

    Require login to access resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  resources :daily_activities
-  resources :projects
-
-  resources :clients
-
-  resources :activitytypes
   mount Upmin::Engine => '/admin'
-#  root to: 'visitors#index'
   devise_for :users
-  resources :users
-
+  resources :users  
+  
   authenticated :user do
+    resources :daily_activities
+    resources :projects
+    resources :clients
+    resources :activitytypes
     root to: 'visitors#index', as: :authenticated_root
   end
+
   root to: redirect('/users/sign_in')
 
 end
